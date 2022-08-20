5ad3abc0008ebe6e293b0f6741be294223ace415
Shelby Bridwell <shelbybridwell@Shelbys-MacBook-Pro.local>
2022-08-20 13:48:53 -0500

    started javascript

---
+++ assets/java.js
@@ -0,0 +1,22 @@
+
+
+const quizDisplay = document.getElementById("display-container");
+let timeLeft = document.querySelector(".time-left");
+let quizContainer = document.getElementById("container");
+let nextBtn = document.getElementById("next-button");
+let countOfQuestions = document.querySelector("number-of-questions");
+let displayContainer = document.getElementById("display-container");
+let scorContainer = document.querySelector("score-container");
+let restart = document.getElementById("restart");
+let userScore = document.getElementById("user-score");
+let startScreen = document.querySelector("start-screen");
+let startButton = document.getElementById("start-button");
+let questionCount;
+let scoreCount = 0;
+let count = 11;
+let countdown;
+
+//Questions and Options array
+
+const quizArray = [{id: "0", questions: ;options[], correct: } ,
+]
